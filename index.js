8e0ce351fca6bbffd60a2c239e4daf974159684a
“Yevgeny <myjetpost@gmail.com>
2020-05-10 13:23:04 +0300

    fix(app): fix rebase conflict

--- index.js
+++ index.js
@@ -3,10 +3,7 @@
  */
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
-<<<<<<< HEAD
 
-=======
->>>>>>> feat(app): initial commit
 import App from './src/App';
 import { name as appName } from './app.json';
 
