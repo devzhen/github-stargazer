afb1267af7d95d95f419f572205136d07869714d
“Yevgeny <myjetpost@gmail.com>
2020-05-10 13:17:59 +0300

    feat(app): add theme

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+export default {
+  appBackground: '#f6f5f3',
+
+  colors: {
+    concrete: '#f3f3f3',
+    error: '#eb7438',
+    gray: '#d3d3d3',
+    lightBlack: '#383838',
+    mineShaft: '#2E2E2E',
+    orange: '#eb9e38',
+    porcelain: '#f6f7f8',
+    silverChalice: '#acacac',
+  },
+};
