c256c8f47002ab3f2d45026d816b3236c8b3ceae
“Yevgeny <myjetpost@gmail.com>
2020-05-10 13:17:18 +0300

    feat(app): add App.tsx and RootContainer

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
+<<<<<<< HEAD
 
+=======
+>>>>>>> feat(app): initial commit
 import App from './src/App';
 import { name as appName } from './app.json';
 
